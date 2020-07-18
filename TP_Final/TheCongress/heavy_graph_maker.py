a08486eeab40206e5a79761d31cde46f694ea26d
melivinograd <melivinograd@gmail.com>
2020-07-17 21:29:05 -0300

    allow only + edges, - edges, move zero

--- TP_Final/TheCongress/heavy_graph_maker.py
+++ TP_Final/TheCongress/heavy_graph_maker.py
@@ -1,5 +1,6 @@
 from networkx_graph import RepresentativesGraph
 from itertools import combinations
+import numpy as np
 
 
 class HeavyGraphMaker:
@@ -35,7 +36,18 @@ class HeavyGraphMaker:
         self.weights = HeavyGraphMaker.DEFINED_WEIGHTS if not config else self._load_config(config)
 
 
-    def create_year_network(self, *years):
+    def move_weights(self, network, *years):
+        old_weights = network.get_sorted_edges_weights()
+        min_weight = min(old_weights.values())
+        print(f'Min weight was {min_weight}')
+        total_votes = 0 #Esto es por si queremos normalizar
+        for year in years:
+            total_votes += len(self.congress.votings[year])
+        new_weights = {e: (w + np.abs(min_weight) + 1) / total_votes for e,w in old_weights.items()}
+        network.set_edge_attributes(new_weights, 'weight')
+
+
+    def create_year_network(self, *years, move_zero = None, positive = None, negative = None):
         representatives = set()
         votings = []
         for year in years:
@@ -44,13 +56,21 @@ class HeavyGraphMaker:
 
         network = RepresentativesGraph(representatives, years)
         for repr_1, repr_2 in combinations(representatives, 2):
-            weight = self._define_weight(repr_1, repr_2, votings)
-            network.add_edge(repr_1, repr_2, weight)
+            weight = self._define_weight(repr_1, repr_2, votings, *years)
+            if positive and weight > 0:
+                network.add_edge(repr_1, repr_2, weight)
+            if negative and weight < 0:
+                network.add_edge(repr_1, repr_2, weight)
+            if not negative and not positive:
+                network.add_edge(repr_1, repr_2, weight)
+
+        if move_zero:
+            self.move_weights(network, *years)
 
         return network
 
 
-    def create_voting_network(self, voting):
+    def create_voting_network(self, voting, positive = None, negative = None):
 
         representatives = voting.get_voters()
         network = RepresentativesGraph(representatives, year)
@@ -62,8 +82,7 @@ class HeavyGraphMaker:
         return network
 
 
-
-    def _define_weight(self, repr_1, repr_2, voting_list):
+    def _define_weight(self, repr_1, repr_2, voting_list, *years):
         ###TODO: si la config dice que 2/3 vale +++
         #le hacemos get y si no lo encuentra lo toma ausente (por si se murio o renuncio)
         return sum([self.weights[(repr_1.votes.get(voting, 'AUSENTE'), repr_2.votes.get(voting, 'AUSENTE'))]
