736144197d88fb82d461eea1ef706d5615bcf83b
melivinograd <melivinograd@gmail.com>
2020-07-17 21:29:50 -0300

    add get attr

--- TP_Final/TheCongress/networkx_graph.py
+++ TP_Final/TheCongress/networkx_graph.py
@@ -1,10 +1,14 @@
 import networkx as nx
 import dill
+import sys
 
 class RepresentativesGraph:
 
     @classmethod
     def save_graph_object(cls, grafo, file_name):
+        if not sys.getrecursionlimit() == 10000:
+            sys.setrecursionlimit(10000)
+            print('Recursion limit was set to 10000')
         with open(file_name, 'wb') as file:
             dill.dump(grafo, file)
 
@@ -20,8 +24,9 @@ class RepresentativesGraph:
     def _create_networkx_graph(self, *years):
         self.graph = nx.Graph()
         self.graph.add_nodes_from(self.representatives)
-        attr_dict = {repr: self._get_attr(repr, years) for repr in self.representatives}
-        nx.set_node_attributes(self.graph, attr_dict)
+        if years:
+            attr_dict = {repr: self._get_attr(repr, years) for repr in self.representatives}
+            nx.set_node_attributes(self.graph, attr_dict)
 
     def add_edge(self, node_1, node_2, weight):
         self.graph.add_edge(node_1, node_2, weight = weight)
@@ -41,6 +46,9 @@ class RepresentativesGraph:
             weights[tuple(sorted(k, reverse = True))] = original_weights[k]
         return weights
 
+    def set_edge_attributes(self, attr_dict, name):
+        nx.set_edge_attributes(self.graph, attr_dict, name)
+
     def get_nodes(self):
         return self.graph.nodes()
 
@@ -56,3 +64,11 @@ class RepresentativesGraph:
 
     def remove_edge(self, edge):
         self.graph.remove_edge(edge[0],edge[1])
+
+    def get_edges(self):
+        return self.graph.edges()
+
+    def copy(self):
+        copy = RepresentativesGraph(self.representatives, None)
+        copy.graph = self.graph.copy()
+        return copy
